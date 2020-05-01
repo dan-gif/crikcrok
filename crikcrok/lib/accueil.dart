774000703056a135de4466c82eca06bc9fa80377
dan-gif <62338080+dan-gif@users.noreply.github.com>
2020-05-01 13:23:12 +0200

    webapp

---
+++ crikcrok/lib/accueil.dart
@@ -0,0 +1,63 @@
+import 'package:crikcrok/commentaires.dart';
+import 'package:flutter/material.dart';
+
+class premier extends StatefulWidget {
+  @override
+  _premierState createState() => _premierState();
+}
+
+// ignore: camel_case_types
+class _premierState extends State<premier> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFFF69D77),
+            Color(0xFF292C36),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, size: 20),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => mainScreen()));
+            },
+          ),
+          title: Text(
+            'Nom du site/application',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Logo',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- crikcrok/lib/main.dart
+++ crikcrok/lib/main.dart
@@ -1,101 +1,12 @@
-
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:crikcrok/commentaires.dart';
+
 
 void main() {
   runApp(
     new MaterialApp(
       title: "Camera APP",
-      home: LandingScreen(),
+      home: mainScreen(),
     ),
   );
-}
-
-class LandingScreen extends StatefulWidget {
-  @override
-  _LandingScreenState createState() => _LandingScreenState();
-}
-
-class _LandingScreenState extends State<LandingScreen> {
-
-  File imageFile;
-
-  _openGallery(BuildContext context)async {
-    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
-    this.setState(() {
-      imageFile = picture;
-    });
-    Navigator.of(context).pop();
-  }
-  _openCamera(BuildContext context)async {
-    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
-    this.setState(() {
-      imageFile = picture;
-    });
-    Navigator.of(context).pop();
-  }
-
-  Future<void> _showChoiceDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext) {
-          return AlertDialog(
-            title: Text("Make a choise!!"),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  Padding(padding: EdgeInsets.all(8.0)),
-                  GestureDetector(
-                    child: Text("Gallery"),
-                    onTap: (){
-                      _openGallery(context);
-                    },
-                  ),
-                  SizedBox(height: 10,),
-                  GestureDetector(
-                    child: Text("Camera"),
-                    onTap: (){
-                      _openCamera(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-          );
-        });
-  }
-  Widget _decideImageViews(){
-    if(imageFile == null){
-      return Text("selectionner image");
-    }
-    else{
-      Image.file(imageFile, width: 400, height: 400);
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Main screen"),
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              _decideImageViews(),
-              RaisedButton(
-                onPressed: () {
-                  _showChoiceDialog(context);
-                },
-                child: Text("Select Image"),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
